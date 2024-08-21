9d84395d490c2983029b787eb9eef648e1d98626
Ashikunnabi <ashikunnabituhin@gmail.com>
2024-08-21 13:47:51 +0600

    add user admin

--- apps/rbac/admin.py
+++ apps/rbac/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.rbac.models.branch import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "is_active", "is_staff", "is_superuser")
+    search_fields = ("username", "email")
+    list_filter = ("is_active", "is_staff", "is_superuser")
+
+
+admin.site.register(User, UserAdmin)
