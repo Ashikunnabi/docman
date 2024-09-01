ba99156187143d9231d98eb8d73f55690929f34f
Ashikunnabi <ashikunnabituhin@gmail.com>
2024-09-01 16:36:24 +0600

    document upload delete

--- static/base/js/index.js
+++ static/base/js/index.js
@@ -1,5 +1,46 @@
-class CustomDropzone{
+class CustomDropzone {
     generate = () => {
-        new AjaxService().getRequest(
-    new Dropzone(".dropzone", { url: "/api/v1/documents/" });}
+        const dropzone = new Dropzone(".dropzone", {
+            url: document_api_url,
+            headers: {
+                "Authorization": `JWT ${getLocalWithExpiry("access")}`
+            },
+            previewTemplate: document.querySelector("#dropzone-template").innerHTML,
+        });
+
+        dropzone.on("success", function (file, response) {
+            $(file.previewTemplate).find('.dz-remove').attr('data-uuid', response.data.uuid);
+        });
+
+        dropzone.on("error", function (file, response) {
+            if (response.message) {
+                $(file.previewTemplate).find('.dz-error-message').text(response.message);
+            }
+        });
+
+        dropzone.on("removedfile", function (file) {
+            let uuid = $(file.previewTemplate).find('.dz-remove').attr('data-uuid');
+            if (!uuid) {
+                console.log('No uuid found');
+                return;
+            }
+            let url = `${document_api_url}${uuid}/`;
+            new AjaxService().deleteRequest(
+                url,
+                function (response) {
+                    console.log('Document deleted successfully');
+                },
+                function (response) {
+                    console.log('Error deleting document');
+                }
+            );
+        });
+    }
+
+    main = () => {
+        this.generate();
+    }
 }
+
+const customDropzone = new CustomDropzone();
+customDropzone.main();
