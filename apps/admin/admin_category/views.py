8b08d9e771445e2841d6dff2fd424ee361a52959
Ashikunnabi <ashikunnabituhin@gmail.com>
2024-09-15 08:52:29 +0600

    permission fix

--- apps/admin/admin_category/views.py
+++ apps/admin/admin_category/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import permission_required
 
 
 
-@permission_required("category.edit_category", raise_exception=True)
+@permission_required("category.change_category", raise_exception=True)
 def category_edit(request, uuid):
     return render(request, "admin_category/category/edit.html", context={"uuid": uuid})
 
